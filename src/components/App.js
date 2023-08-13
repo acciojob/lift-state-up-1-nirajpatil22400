5dc8d51822d66e8a3767fe2a9aaaeb9ac16b1bd1
NirajPatil <nirajpatil@22400.com>
2023-08-13 19:50:56 +0530

    my snapshot

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
+import React, { useState } from "react";
+import "./../styles/App.css";
+import ChildComponent from "./ChildComponent";
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const [showModal, setShowModal] = useState(false);
 
-const App = () => {
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div class="parent">
+      <h1>Parent Component</h1>
+      <ChildComponent showModal={showModal} handleClick={handleClick} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
